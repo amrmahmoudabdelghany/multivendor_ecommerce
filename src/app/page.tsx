a453204120c19e64edc598153fb2d7d60e7e4e1b
Amr Mahmoud <amr.mahmoud.abdelghany51@gmail.com>
2025-04-27 14:40:19 +0300

    basic setup , shadcn , theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-bold text-blue-500">Welcome to the course</h1>
+    <div className="p-5">
+      <div className="flex justify-end w-100">
+        <ThemeToggle />
+      </div>
+      <h1 className="font-barlow text-blue-500">Welcome to the course</h1>
+      <h1 className="  text-blue-500">Welcome to the course</h1>
+
+      <Button variant="destructive">Click Here</Button>
     </div>
   );
 }
